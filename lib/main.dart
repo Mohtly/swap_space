75aa3bfb578c409f684e8870e8fc9a4c1d3b00bb
Maxwell Thom <maxwell_thom@nevada.unr.edu>
2019-11-10 01:51:36 -0800

    ripped of raywendlich instead

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,16 @@
 import 'package:flutter/material.dart';
 import './Login.dart';
-import './Take_Picture_Screen.dart';
-import 'package:camera/camera.dart';
-import 'dart:async';
+import './camera_screen.dart';
 
 
-Future<void> main() async {
-  final cameras = await availableCameras();
-  // Get a specific camera from the list of available cameras.
-  final firstCamera = cameras.first;
-  return runApp(
-    MaterialApp(
-      home: TakePictureScreen(
-        // Pass the appropriate camera to the TakePictureScreen widget.
-        camera: firstCamera,
-      ),
-    ),
-  );
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CameraScreen(),
+    );
+  }
 }
-
-// class MyApp extends StatelessWidget {
-
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Login(),
-//     );
-//   }
-// }
