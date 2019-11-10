51591d1a0f9c97d5de137b4029fbfa2bc25fb3b9
Maxwell Thom <maxwell_thom@nevada.unr.edu>
2019-11-09 23:25:26 -0800

    built card view, added assets folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import './Swap_Search.dart';
 import './Login.dart';
 
 
@@ -10,7 +10,7 @@ class HackYo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Login(),
+      home: SwapSearch(),
     );
   }
 }
