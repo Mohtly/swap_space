86495f2bdb9359c6548760fdb7d73fc0c1d4642a
Maxwell Thom <maxwell_thom@nevada.unr.edu>
2019-11-10 01:03:04 -0800

    add graphql to project and set it up

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,41 @@
 import 'package:flutter/material.dart';
 import './Login.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
+void main() {
+  final HttpLink httpLink = HttpLink(
+    uri: 'https://api.github.com/graphql',
+  );
 
+  final AuthLink authLink = AuthLink(
+    getToken: () async => 'Bearer e87075ff040ae349900a77a0cc86a76a982b7ae7',
+  );
 
-void main() => runApp(MyApp());
+  final Link link = authLink.concat(httpLink);
+
+  ValueNotifier<GraphQLClient> client = ValueNotifier(
+    GraphQLClient(
+      cache: InMemoryCache(),
+      link: link,
+    ),
+  );
+
+  return runApp(MyApp(client: client));
+}
 
 class MyApp extends StatelessWidget {
+  final ValueNotifier<GraphQLClient> client;
+  const MyApp({Key key, this.client}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Login(),
+    return GraphQLProvider(
+      client: client,
+      child: CacheProvider(
+        child: MaterialApp(
+          home: Login(),
+        ),
+      ),
     );
   }
 }
